67eb18d21adece35b3737203447ae3c8af9bfde1
Fabian Mosso Chavez <fmosso@klabs.cl>
2018-07-26 14:06:12 -0400

    delete compile files

--- graphql/built/credentials.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mailCredential = {
-    mailUser: 'awsfreetrial2018@gmail.com',
-    mailPassword: 'AWS@2018 '
-};
