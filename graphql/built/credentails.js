cb40ee9dc6eb66df61b45f6da1973cf35707e27f
Fabian Mosso Chavez <fmosso@klabs.cl>
2018-08-01 11:19:07 -0400

    remove two compilation folders

--- graphql/built/credentails.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mailUser = 'awsfreetrial2018@gmail.com';
-exports.mailPassword = 'AWS@2018 ';
--- graphql/built/dummy.js
+++
@@ -1,5 +0,0 @@
-const promesa = new Promise((resolve, reject) => {
-    resolve(false);
-});
-promesa.then();
-console.log(promesa);
